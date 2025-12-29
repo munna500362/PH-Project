c806ee38571bc193ffff2231a1c6fd94d6232b35
mehedi hasan munna <mehedihasan500362@gmail.com>
2025-12-29 20:39:35 +0600

    video section

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,10 @@ function catagory (){
     .then((data) => loadData(data.categories))
 }
 
-const videoSection = () => {
+const videofetchSection = () => {
     fetch ('https://openapi.programming-hero.com/api/phero-tube/videos')
     .then ((res) => res.json())
-    .then ((data) => console.log(data))
+    .then ((data) => VideoCategorySection(data.videos))
 
 }
 // category
@@ -31,6 +31,68 @@ function loadData(categories){
         categoriContainer.appendChild(div);
 }}
 
+
+const VideoCategorySection = (videos) => {
+    const videoContainer = document.getElementById("video-container");
+    // console.log(videos);
+   videos.forEach(video => {
+    const div = document.createElement("div");
+// 
+
+    
+    div.innerHTML = `
+    
+    
+    <div class="card bg-base-100 shadow-sm">
+                <figure class="relative">
+                    <img
+                    src="${video.thumbnail}"
+                    alt="${video.title}" />
+                    <span class="absolute bottom-2 right-2 bg-black text-white text-xs px-2 py-1 rounded">${video.others.posted_date}</span>
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">${video.title}</h2>
+                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+    
+    
+    
+    `
+    videoContainer.appendChild(div);
+    
+   });
+}
+
 catagory();
 
-videoSection();
+videofetchSection();
+
+
+// authors
+// : 
+// [{…}]
+// category_id
+// : 
+// "1001"
+// description
+// : 
+// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// others
+// : 
+// {views: '100K', posted_date: '16278'}
+// thumbnail
+// : 
+// "https://i.ibb.co/L1b6xSq/shape.jpg"
+// title
+// : 
+// "Shape of You"
+// video_id
+// : 
+// "aaaa"
+// [[Prototype]]
+// : 
+// Object
